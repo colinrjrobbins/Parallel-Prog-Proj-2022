e4ec6f0c5e37338e6dfad9aed06694ff82f69de5
Colin Robbins <42009797+colinrjrobbins@users.noreply.github.com>
2022-11-19 18:19:55 -0500

    Initial Commit, basic layout structure.

---
+++ modules/FileData.py
@@ -0,0 +1,18 @@
+def get_file_data(filename : str):
+    """ Function: used to open and convert the file into a python dict and return data """
+    name_file = open(filename, 'r')
+
+    # Open the file and split the data by new lines.
+    file_data = name_file.read()
+    file_string = file_data.split('\n')
+    name_dict = {}
+
+    # Initialize X as a key counter
+    x = 0
+
+    # Loop through and assign first and last name to each key
+    for name in file_string:
+        name_dict[x] = {"first":name.split(' ')[0],"last":name.split(' ')[1]}
+        x = x + 1
+
+    return name_dict
---
+++ modules/Parallel.py
@@ -0,0 +1,11 @@
+import time
+
+class ParallelSort:
+    def __init__(self, name_dict : dict):
+        self.name_dict = name_dict
+
+    def initialize_threads(self):
+        pass
+
+    def sort_names(self):
+        pass
