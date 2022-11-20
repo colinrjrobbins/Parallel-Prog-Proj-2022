9b341e7a6148181bcb8666449db54fccda07711b
Colin Robbins <42009797+colinrjrobbins@users.noreply.github.com>
2022-11-20 16:46:54 -0500

    Started Quicksort
    - Have simple print statement for quick data comparison.
    - Will order by last name right now, need to finalize on last then first name sort.

--- modules/Sequential.py
+++ modules/Sequential.py
@@ -2,7 +2,43 @@ import time
 
 class SequentialSort:
     def __init__(self, name_dict : dict):
-        self.name_dict = name_dict
+        self.nd = name_dict
+
+    def partition(self, low: int, high: int, firstLast: str):
+        pivot = self.nd[high][firstLast]
+
+        if firstLast == 'last':
+            lastFirst = 'first'
+
+        i = low - 1
+
+        for j in range(low, high):
+            if self.nd[j][firstLast] <= pivot:
+                i = i + 1
+                (self.nd[i],self.nd[j]) = (self.nd[j], self.nd[i])
+
+        (self.nd[i+1],self.nd[high]) = (self.nd[high],self.nd[i+1])
+        
+        return i + 1
+
+    def quicksort(self, low: int, high: int, firstLast: str):
+        if low < high:
+            part = self.partition(low, high, firstLast)
+
+            self.quicksort(low, part - 1, firstLast)
+
+            self.quicksort(part+1, high, firstLast)
 
     def sort_names(self):
-        pass
+        print("\nSHOWING TOP 10 FROM FILE")
+        for i in range(0,10):
+            print(self.nd[i]['first'] + ' ' + self.nd[i]['last'])
+
+        input("\nPress enter to sort...")
+
+        self.quicksort(0,len(self.nd)-1, 'last')
+        #self.quicksort(0,len(self.nd)-1, 'first')
+
+        print("\nSHOWING TOP 10 FROM SORT")
+        for i in range(0,10):
+            print(self.nd[i]['first'] + ' ' + self.nd[i]['last'])
